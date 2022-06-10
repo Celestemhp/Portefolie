8f0355d4874953f18843bbd8c6cd75853d342776
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2022-06-10 03:12:25 +0200

    mediaq

---
+++ script.js
@@ -0,0 +1,12 @@
+// klik variabel
+// const klik = document.querySelector(".mappe");
+
+//Lyt efter klik på element
+// mappe.addEventListener(".mappe", click);
+
+// // lyd til klik
+// function click() {
+//     console.log("lyd");
+//     document.querySelector("#klikLyd").play();
+//     document.querySelector("#klikLyd").volume = 1.5;
+// }
