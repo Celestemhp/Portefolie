1354a70eab2b058b3ba032d4f7bfeb0c7e70e60e
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2022-06-13 16:04:05 +0200

    alt virker

--- script.js
+++ script.js
@@ -9,37 +9,37 @@ function click() {
     document.querySelector("#klikLyd").volume = 1.5;
 }
 
-document.querySelector("body").addEventListener("mousedown", click); {
-    if (click.pressure == 0) {
-      // No pressure
-      process_no_pressure(click);
-    } else if (click.pressure == 1) {
-      // Maximum pressure
-      process_max_pressure(click);
-    } else {
-      // Default
-      process_pressure(click);
-    }
-  }
+// document.querySelector("body").addEventListener("mousedown", click); {
+//     if (click.pressure == 0) {
+//       // No pressure
+//       process_no_pressure(click);
+//     } else if (click.pressure == 1) {
+//       // Maximum pressure
+//       process_max_pressure(click);
+//     } else {
+//       // Default
+//       process_pressure(click);
+//     }
+//   }
 
 
-// Dropdown
-// klik variabel
-const visNav = document.querySelector("#visNav");
+// // Dropdown
+// // klik variabel
+// const visNav = document.querySelector("#visNav");
 
-// When the user clicks on the button - toggle between hiding and hideing//
-function visMenuBtn() {
-document.getElementById("#visNav").classList.toggle("hide");
-}
+// // When the user clicks on the button - toggle between hiding and hideing//
+// function visMenuBtn() {
+// document.getElementById("#visNav").classList.toggle("hide");
+// }
 
-function visMenuBtn() {
-if (typeof menu.style.display === "menu") {
-    menu.style.display = "menu";
-} 
-else {
-    menu.style.display = "hide";
-}
-}
+// function visMenuBtn() {
+// if (typeof menu.style.display === "menu") {
+//     menu.style.display = "menu";
+// } 
+// else {
+//     menu.style.display = "hide";
+// }
+// }
 
 // const button = document.querySelector('button');
 
